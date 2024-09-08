8e7a117b944d46f784153d63fc2b216d708b4049
ravithakur96 <ravishivam96@gmail.com>
2024-09-08 16:47:35 +0530

    classroom

---
+++ classroom/script.js
@@ -0,0 +1,9 @@
+let heading = document.getElementById("heading");
+console.log(heading);
+
+let headings = document.getElementsByClassName("heading2");
+console.dir(headings);
+console.log(headings);
+
+let parahs = document.getElementsByTagName("p");
+console.dir(parahs);   
